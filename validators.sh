19f456ddc6dc4221378ebdef80e97502366f8428
EvilJordan <jrh@jordanholberg.com>
2026-04-23 22:05:21 -0700

    database filename uses .env

--- validators.sh
+++ validators.sh
@@ -7,7 +7,7 @@ echo "Extracting relevant validator data into CSV..."
 jq -r ' (["vindex","balance","status","pubkey","withdrawal_credentials"]), (.data[] | [ .index, .balance, .status, .validator.pubkey, .validator.withdrawal_credentials ]) | @csv ' validators.json > validators.csv
 
 echo "Importing validators into db..."
-sqlite3 soloStakers.db ".mode csv" "$(head -n1 validators.csv | awk -F, '{printf "DROP TABLE validators; CREATE TABLE validators ("; for(i=1;i<=NF;i++){col=$i; gsub(/\"/, "", col); if(col=="vindex"){printf "\"%s\" INTEGER", col}else{printf "\"%s\" TEXT", col} if(i<NF) printf ", "} printf ");"}')" ".import --skip 1 validators.csv validators"
+sqlite3 $DATABASE ".mode csv" "$(head -n1 validators.csv | awk -F, '{printf "DROP TABLE validators; CREATE TABLE validators ("; for(i=1;i<=NF;i++){col=$i; gsub(/\"/, "", col); if(col=="vindex"){printf "\"%s\" INTEGER", col}else{printf "\"%s\" TEXT", col} if(i<NF) printf ", "} printf ");"}')" ".import --skip 1 validators.csv validators"
 
 echo "Cleaning up..."
 rm validators.json
