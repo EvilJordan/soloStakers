e70e126e0c18d4ad3f78054852bbe6e0baf7b76d
EvilJordan <jrh@jordanholberg.com>
2026-04-23 22:09:15 -0700

    filename variables

--- validators.sh
+++ validators.sh
@@ -1,15 +1,16 @@
 source .env
+VALIDATORSTEMPFILE=validatorsTemp.csv
 
 echo "Retrieving latest validator list from RPC node..."
-curl $CLRPCADDRESS:$CLRPCPORT/eth/v1/beacon/states/head/validators > validators.json
+curl $CLRPCADDRESS:$CLRPCPORT/eth/v1/beacon/states/head/validators > $VALIDATORSFILE
 
 echo "Extracting relevant validator data into CSV..."
-jq -r ' (["vindex","balance","status","pubkey","withdrawal_credentials"]), (.data[] | [ .index, .balance, .status, .validator.pubkey, .validator.withdrawal_credentials ]) | @csv ' validators.json > validators.csv
+jq -r ' (["vindex","balance","status","pubkey","withdrawal_credentials"]), (.data[] | [ .index, .balance, .status, .validator.pubkey, .validator.withdrawal_credentials ]) | @csv ' $VALIDATORSFILE > $VALIDATORSTEMPFILE
 
 echo "Importing validators into db..."
-sqlite3 $DATABASE ".mode csv" "$(head -n1 validators.csv | awk -F, '{printf "DROP TABLE validators; CREATE TABLE validators ("; for(i=1;i<=NF;i++){col=$i; gsub(/\"/, "", col); if(col=="vindex"){printf "\"%s\" INTEGER", col}else{printf "\"%s\" TEXT", col} if(i<NF) printf ", "} printf ");"}')" ".import --skip 1 validators.csv validators"
+sqlite3 $DATABASE ".mode csv" "$(head -n1 $VALIDATORSTEMPFILE | awk -F, '{printf "DROP TABLE validators; CREATE TABLE validators ("; for(i=1;i<=NF;i++){col=$i; gsub(/\"/, "", col); if(col=="vindex"){printf "\"%s\" INTEGER", col}else{printf "\"%s\" TEXT", col} if(i<NF) printf ", "} printf ");"}')" ".import --skip 1 $VALIDATORSTEMPFILE validators"
 
 echo "Cleaning up..."
-rm validators.json
-rm validators.csv
+rm $VALIDATORSFILE
+rm $VALIDATORSTEMPFILE
 echo "Done!"
