e73af5574b2bbe54616a288e13ae373190e89e7b
EvilJordan <jrh@jordanholberg.com>
2026-05-06 22:55:53 -0700

    add conditional if exists to table drop

--- validators.sh
+++ validators.sh
@@ -9,7 +9,7 @@ echo "Extracting relevant validator data into CSV..."
 jq -r ' (["vindex","balance","status","pubkey","withdrawal_credentials"]), (.data[] | [ .index, .balance, .status, .validator.pubkey, .validator.withdrawal_credentials ]) | @csv ' $VALIDATORSTEMPFILEJSON > $VALIDATORSTEMPFILE
 
 echo "Importing validators into db..."
-sqlite3 $DATABASE ".mode csv" "$(head -n1 $VALIDATORSTEMPFILE | awk -F, '{printf "DROP TABLE validators; CREATE TABLE validators ("; for(i=1;i<=NF;i++){col=$i; gsub(/\"/, "", col); if(col=="vindex"){printf "\"%s\" INTEGER", col}else{printf "\"%s\" TEXT", col} if(i<NF) printf ", "} printf ");"}')" ".import --skip 1 $VALIDATORSTEMPFILE validators"
+sqlite3 $DATABASE ".mode csv" "$(head -n1 $VALIDATORSTEMPFILE | awk -F, '{printf "DROP TABLE IF EXISTS validators; CREATE TABLE validators ("; for(i=1;i<=NF;i++){col=$i; gsub(/\"/, "", col); if(col=="vindex"){printf "\"%s\" INTEGER", col}else{printf "\"%s\" TEXT", col} if(i<NF) printf ", "} printf ");"}')" ".import --skip 1 $VALIDATORSTEMPFILE validators"
 
 echo "Cleaning up..."
 rm $VALIDATORSTEMPFILEJSON
