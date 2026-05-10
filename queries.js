85b1c8e55901333c9a71ce9934eb70fef88c9cd7
EvilJordan <jrh@jordanholberg.com>
2026-05-09 23:05:27 -0700

    get all statuses for validators

--- queries.js
+++ queries.js
@@ -42,8 +42,10 @@ if (firstMethod) {
 		}
 
 		// for each deposit_address, pull updated withdrawal_credentials from the latest validator set and add to our lookup object, along with other metadata
-		query = DB.prepare("SELECT status, COUNT(status) AS numValidators, GROUP_CONCAT(DISTINCT vindex) AS vindices, SUM(balance) AS balance, withdrawal_credentials AS withdrawal_address FROM (SELECT DISTINCT(pubkey) AS pubkey1 FROM deposits WHERE deposit_address = '" + depositAddress + "'), validators WHERE pubkey1 = pubkey").all();
-		depositAddressData.status = query[0].status; // what if only some of the validators are active_ongoing?
+		query = DB.prepare("SELECT GROUP_CONCAT(DISTINCT status) AS status, COUNT(status) AS numValidators, GROUP_CONCAT(DISTINCT vindex) AS vindices, SUM(balance) AS balance, withdrawal_credentials AS withdrawal_address FROM (SELECT DISTINCT(pubkey) AS pubkey1 FROM deposits WHERE deposit_address = '" + depositAddress + "'), validators WHERE pubkey1 = pubkey").all();
+		if (query[0].status?.length > 0) {
+			depositAddressData.status = query[0].status.split(","); // what if only some of the validators are active_ongoing?
+		}
 		depositAddressData.numValidators = query[0].numValidators;
 		depositAddressData.balance = query[0].balance;
 		depositAddressData.withdrawalAddresses.push(...[ query[0].withdrawal_address ].filter(address => withdrawal_addressRegex.test(address))); // filter withdrawal_address (withdrawal_credentials) and only pull out ones that are 0x01 or 0x02
@@ -56,6 +58,7 @@ if (firstMethod) {
 				depositAddressData.graffiti = query[0].graffiti.split(",");
 			}
 		}
+		// if our status field contains anything other than active_ongoing, we need to create a new object to determine which indexes, pubkeys, and withdrawal_credentials are not part of the active set?
 		if (i == depositAddresses.length - 1) { comma = ''; }
 		stream.write('"' + depositAddress + '": ' + JSON.stringify(depositAddressData, null, '\t') + comma + '\n');
 		progressBar.update(i);
