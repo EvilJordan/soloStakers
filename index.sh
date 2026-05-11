350f04be5ef5663597d1b6ce608a4b655785dfab
EvilJordan <jrh@jordanholberg.com>
2026-05-11 00:47:33 -0700

    fixed missing .env and added conditional dropping at start

--- index.sh
+++ index.sh
@@ -1,8 +1,17 @@
+source .env
+
 echo "Creating database indexes..."
-sqlite3 $DATABASE "CREATE INDEX IF NOT EXISTS withdrawal_credentials_index ON validators (withdrawal_credentials); \
+sqlite3 $DATABASE \
+"DROP INDEX IF EXISTS withdrawal_credentials_index; \
+DROP INDEX IF EXISTS pubkey_index; \
+DROP INDEX IF EXISTS withdrawal_credentials_deposits_index; \
+DROP INDEX IF EXISTS deposit_address_deposits_index; \
+DROP INDEX IF EXISTS pubkey_deposits_index; \
+DROP INDEX IF EXISTS vindex_index; \
+CREATE INDEX IF NOT EXISTS withdrawal_credentials_index ON validators (withdrawal_credentials); \
 CREATE INDEX IF NOT EXISTS pubkey_index ON validators (pubkey); \
 CREATE INDEX IF NOT EXISTS withdrawal_credentials_deposits_index ON deposits (withdrawal_credentials); \
 CREATE INDEX IF NOT EXISTS deposit_address_deposits_index ON deposits (deposit_address); \
 CREATE INDEX IF NOT EXISTS pubkey_deposits_index ON deposits (pubkey); \
-CREATE INDEX IF NOT EXISTS vindex_index ON graffiti (vindex);" ".quit"
+CREATE INDEX IF NOT EXISTS vindex_index ON graffiti (vindex);"
 echo "Done!"
